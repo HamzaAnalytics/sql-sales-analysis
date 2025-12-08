d75a1a4780c00c1b877fa237e4597658c9ae8fb8
Hamza Humayun <hamza.humayun@rennes-sb.com>
2025-12-08 15:46:42 +0100

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO customers VALUES
+(1, 'Ali', 'France'),
+(2, 'Sara', 'Germany'),
+(3, 'John', 'UK');
+
+INSERT INTO products VALUES
+(1, 'Laptop', 1200),
+(2, 'Headphones', 80),
+(3, 'Keyboard', 40);
+
+INSERT INTO sales VALUES
+(1, 1, 1, 1, '2024-01-10'),
+(2, 2, 2, 2, '2024-01-12'),
+(3, 3, 3, 3, '2024-01-15'),
+(4, 1, 2, 1, '2024-02-01');
